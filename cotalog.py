b5485ab04afdfb0420dcff8a4523ec63555f603d
LinkxWan <94342617+Linkxwan@users.noreply.github.com>
2022-01-07 04:36:04 +0600

    cotalog.py

---
+++ cotalog.py
@@ -0,0 +1,21 @@
+catalogue = {
+    'microphone':1500,
+    'air-pods':4000,
+    'beats':8000,
+    'samsung a5': 10500,
+    'Acer ASPIRE e15': 30000, 
+    'hard drive':6400,
+    'iphone 8s': 16000,
+    'iphone X': 40000, 
+    'mouse RX7':8000,
+    'hikvision laland':2280,
+    'kocom': 3990, 
+    'HIKVISION DS-KD-DIS': 5410,
+    'commaX cdv':9560, 
+    'TP-LINK TL-WR841N':1450,
+    'hoco m60': 120, 
+    'SVEN ap':290,
+    'panasonic': 32460,
+    'Falcon sdd ADATA':6650, 
+    'Apple Watch':8000
+}
